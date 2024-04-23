07a3b26d8ff9d6ee356b421bdd3312cc6ec095ba
muthamikennedy001 <muthamikennedy0@gmail.com>
2024-04-23 21:45:11 +0300

    port

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import "./app.scss";
+import NavBar from "./components/navbar/NavBar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <NavBar />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contact</section>
+    </div>
+  );
 };
 
 export default App;
